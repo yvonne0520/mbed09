e9b00193ed1043eff1e85d10182f75cbfd951543
yvonne0520 <yvonne8805200520@gmail.com>
2020-05-13 18:29:20 +0800

    uplode

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,3 +1,4 @@
+
 #include "mbed.h"
 #include "mbed_rpc.h"
 
@@ -5,26 +6,36 @@
  *  This example program has been updated to use the RPC implementation in the new mbed libraries.
  *  This example demonstrates using RPC over serial
 **/
-RpcDigitalOut myled1(LED1,"myled1");
-//RpcDigitalOut myled2(LED2,"myled2");
-RpcDigitalOut myled2(LED3,"myled2");
+/*RpcDigitalOut myled1(LED1, "myled1");
+RpcDigitalOut myled2(LED2, "myled2");
+RpcDigitalOut myled3(LED3, "myled3");*/
+Ticker timeup;
+DigitalOut RED(LED1);
+DigitalOut BLUE(LED3);
 Serial pc(USBTX, USBRX);
-void LEDControl(Arguments *in, Reply *out);
-RPCFunction rpcLED(&LEDControl, "LEDControl");
-double x, y;
+void Control(Arguments *in, Reply *out);
+void blink();
+RPCFunction rpcLED(&Control, "Control");
+double x;
+int on_or = 0;
 
-
-int main() {
-
+int main()
+{
+    RED  = 0;
+    BLUE = 1;
+    timeup.attach(&blink, 0.3);
     //The mbed RPC classes are now wrapped to create an RPC enabled version - see RpcClasses.h so don't add to base class
 
     // receive commands, and send back the responses
     char buf[256], outbuf[256];
-    while(1) {
+    while (1)
+    {
         memset(buf, 0, 256);
-        for (int i = 0; ; i++) {
+        for (int i = 0;; i++)
+        {
             char recv = pc.getc();
-            if (recv == '\r') {
+            if (recv == '\r')
+            {
                 pc.printf("\r\n");
                 break;
             }
@@ -39,24 +50,16 @@ int main() {
 }
 
 // Make sure the method takes in Arguments and Reply objects.
-void LEDControl (Arguments *in, Reply *out)   {
-    bool success = true;
+void Control(Arguments *in, Reply *out)
+{   x = in->getArg<double>();
+    on_or = x;
+}
 
-    // In this scenario, when using RPC delimit the two arguments with a space.
-    x = in->getArg<double>();
-    y = in->getArg<double>();
-
-    // Have code here to call another RPC function to wake up specific led or close it.
-    char buffer[200], outbuf[256];
-    char strings[20];
-    int led = x;
-    int on = y;
-    int n = sprintf(strings, "/myled%d/write %d", led, on);
-    strcpy(buffer, strings);
-    RPC::call(buffer, outbuf);
-    if (success) {
-        out->putData(buffer);
-    } else {
-        out->putData("Failed to execute LED control.");
+void blink () 
+{
+    if (on_or == 1)
+    {
+        RED  = !RED;
+        BLUE = !BLUE;
     }
 }
